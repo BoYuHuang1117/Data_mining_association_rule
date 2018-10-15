cca20f6f6b77d35b9d45e6126b89ab7e269d941a
BoYu Huang <38172621+BoYuHuang1117@users.noreply.github.com>
2018-10-15 23:08:05 +0800

    Add files via upload
    Add frequent one-item set generate function

--- Fp_growth.py
+++ Fp_growth.py
@@ -27,3 +27,53 @@ def loadIBM(filename='data.ntrans_0.1.nitems_0.01'):
     
     return dataset_list
     
+def generateL1(dataset_list,min_support = 0.4):
+    # create L1 in a huge list 
+    L1 = []
+    min_support = 0.4*len(dataset_list)
+    for itemsets in dataset_list:
+        for item in itemsets:
+            if not item in L1:
+                L1.append(item)
+    
+    # Dictionary 
+    dictL1 = {}
+    for i in range(len(L1)):
+        count = 0
+        for transaction in dataset_list:
+            if L1[i] in transaction:
+                count += 1
+        if count >= min_support:
+            dictL1[L1[i]] = count
+        
+    # Sorting the dictonary by value 
+    import operator
+    # A big list contains multiple tuple
+    sorted_dictL1 = sorted(dictL1.items(),key=operator.itemgetter(1),reverse=True)
+    
+    # convert back to dictionary without losing the order 
+    from collections import OrderedDict
+    sorted_dictL1 = OrderedDict(sorted_dictL1)
+    
+    return sorted_dictL1
+    
+def new_dataset(dataset_list,sorted_dictL1):
+    # Utilize keys in sorted_dictL1 to elminate non-frequent item in each transaction
+    # And put transaction in sorted_dictL1 order
+    freq_item_one = list(sorted_dictL1.keys())
+    new_dataset_list = []
+    for transaction in dataset_list:
+        new_tran = list([element for element in freq_item_one if element in transaction])
+        new_dataset_list.append(new_tran)
+    
+    return new_dataset_list
+    
+def :
+    
+    
+    return 
+    
+def FPTree(new_dataset_list,):
+    
+    
+    return 
